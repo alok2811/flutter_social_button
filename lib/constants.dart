4aabfa1581efb3532078441e510a76981fbe5abf
Alok Dubey <72010020+alok2811@users.noreply.github.com>
2022-01-31 16:57:08 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 
+// This is facebook background color
 const Color facebookColor = Color(0xff39579A);
+
+// This is twitter background color
 const Color twitterColor = Color(0xff00ABEA);
+
+// This is instagram background color
 const Color instaColor = Color(0xffBE2289);
+
+// This is whatsapp background color
 const Color whatsappColor = Color(0xff075E54);
+
+// This is linkdin background color
 const Color linkedinColor = Color(0xff0085E0);
+
+// This is github background color
 const Color githubColor = Color(0xff202020);
+
+// This is apple background color
 const Color appleColor = Color(0xff000000);
+
+// This is google background color
 const Color googleColor = Color(0xffDF4A32);
+
+// This is phone background color
 const Color phoneColor = Color(0xff455a64);
